b008f3d733fd837db732eced6938116095cb0303
Tiler <youremail@yourdomain.com>
2026-05-07 07:24:48 -0700

    t

--- js/demo.js
+++ js/demo.js
@@ -1,11 +1,4 @@
-/**
- * Particleground demo
- * @author Jonathan Nicol - @mrjnicol
- */
-
-document.addEventListener('DOMContentLoaded', function () {
-  particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
-  });
-}, false);
+particleground(document.getElementById('particles'), {
+  dotColor: '#5cbdaa',
+  lineColor: '#5cbdaa'
+});
